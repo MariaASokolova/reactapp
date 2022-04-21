6e38033eed9295561b3e640df2f62137b291d9b8
MariaASokolova <51476700+MariaASokolova@users.noreply.github.com>
2022-04-21 19:44:55 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather App</h1>
-        <a
-          href="https://github.com/MariaASokolova/reactapp"
-          className="linktocode"
-        >
-          Open source by MariaASokolova
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>Weather App</h1>
+          <a
+            href="https://github.com/MariaASokolova/reactapp"
+            className="linktocode"
+          >
+            Open source by MariaASokolova
+          </a>
+        </header>
+      </div>
     </div>
   );
 }
